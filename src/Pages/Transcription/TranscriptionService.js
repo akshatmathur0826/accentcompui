238676b71541d03b3aef65162f8bd98514b23e81
Akshat Mathur <akshatmathur0826@gmail.com>
2025-06-23 22:10:51 +0530

    api changes

--- src/Pages/Transcription/TranscriptionService.js
+++ src/Pages/Transcription/TranscriptionService.js
@@ -1,48 +1,37 @@
 import axios from 'axios';
 
-export const listenAudio = async (TTS,voice_id) => {
+export const listenAudio = async (TTS, voice_id) => {
     try {
-
-        const response = await fetch(`https://api.elevenlabs.io/v1/text-to-speech/${voice_id}?output_format=mp3_44100_128`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "xi-api-key": "sk_7b2a24bd403219e500ab4ee700e412989d1b87771b71d862",
-            },
-            body: JSON.stringify({
-                text: TTS,
-                model_id: "eleven_multilingual_v2",
-                voice_settings: {
-                    speed: 1.2,
-                },
-            }),
+        const response = await axios.get('http://localhost:5000/api/getElevenLabsResponse', {
+            params: { TTS: TTS, voice_id: voice_id },
+            responseType: 'blob'
         });
+        console.log(response);
+       
+        return response.data;
 
-        if (!response.ok) {
-            throw new Error("Failed to fetch audio");
-        }
-        return response;
-        
     } catch (error) {
         console.error("Error playing audio:", error);
     }
 }
 
 export const getGeminiResponse = async () => {
-    try{
+    try {
         const response = await axios.get('http://localhost:5000/api/getOpenAIResponse');
-    // Use response.data.message as needed
-    return response.data.message;
+        // Use response.data.message as needed
+        return response.data.message;
     }
     catch (error) {
         console.error("Error fetching Gemini response:", error);
     }
 }
 
-export const sendUserResponse = async (userResponse,groundTruth) => {
+export const sendUserResponse = async (userResponse, groundTruth) => {
     try {
-        const response = await axios.post('http://localhost:8000/userResponse', { userInput: userResponse,
-      groundTruth: groundTruth, });
+        const response = await axios.post('http://localhost:8000/userResponse', {
+            userInput: userResponse,
+            groundTruth: groundTruth,
+        });
         return response.data;
     } catch (error) {
         console.error("Error sending user response:", error);
